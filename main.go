abc8b1edddd115e9b8a1e692cb5c32eb2f3b21df
abdelrahman027 <116889922+abdelrahman027@users.noreply.github.com>
2025-08-13 22:18:14 +0300

    Adding main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Greeting(n string) {
+	fmt.Println("Hello", n)
+}
+
+func CycleNames(n []string, f func(string)) {
+	for _, value := range n {
+		f(value)
+	}
+}
+func main() {
+	names := []string{"Ahmed", "Body", "Ali"}
+	CycleNames(names, Greeting)
+}
